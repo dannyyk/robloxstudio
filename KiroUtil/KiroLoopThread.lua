6e6465536c51337979ecec07fec69f13f287e7e2
raydjs <68088905+raydjs@users.noreply.github.com>
2023-06-27 22:35:20 -0600

    Update KiroLoopThread.lua
    added examples

--- KiroUtil/KiroLoopThread.lua
+++ KiroUtil/KiroLoopThread.lua
@@ -1,14 +1,28 @@
---to delay or add any wait() you would have to use tick() or os.time()
+--[[to delay or add any wait() you would have to use tick() or os.time()
 
---@start() a new loop
+@start() a new loop
 
---@fill() insert a new function inside
---@sparam requires a given name index space to clear the function later.
---@fparam requires a function to fill the loop to run through
+@fill() insert a new function inside
+@sparam requires a given name index space to clear the function later.
+@fparam requires a function to fill the loop to run through
 
---@get() returns the table of function table to index and remove them later on.
+@get() returns the table of function table to index and remove them later on.
 
---@terminate() Ends the loop.
+@terminate() Ends the loop.
+
+example usage:
+    Server or Client:
+        local KiroLoop = require(KiroLoop)
+        KiroLoop:Start()
+
+        KiroLoop:Fill("any_name", function()
+            print("loop 1")
+        end)
+
+        KiroLoop:Fill("any_name1", function()
+            print("loop 2")
+        end)
+]]
 
 local RunService = game:GetService("RunService")
 local KiroLoop = {}
