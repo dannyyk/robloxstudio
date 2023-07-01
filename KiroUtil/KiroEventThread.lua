fb8f5c6b832a376244ae99797a1becd96669131e
raydjs <68088905+raydjs@users.noreply.github.com>
2023-06-30 18:47:47 -0600

    Update KiroEventThread.lua
    yessir

--- KiroUtil/KiroEventThread.lua
+++ KiroUtil/KiroEventThread.lua
@@ -1,9 +1,48 @@
 --!nonstrict
-local Connection = newproxy(true)
-local connectionTable = getmetatable(Connection)
 
-local globalFunction = newproxy(true)
-local globalTableF = getmetatable(globalFunction)
+--[[
+
+    -@kiro-event-wrapper
+    -@authored by: Kiro
+    -@class: Kiraps
+
+    GLOBALS:
+        addGlobal -- adds another function to the given **name_space**
+            - Goes to globalTable
+            - Actives in Event
+
+        deleteGlobal -- removes a function to the given **name_space** and **name_thread**
+            - Goes to globalTable
+            - Actives in Event
+
+        Disconnect -- Destroys an event when **name_space** is given
+            - Goes to connectionTable
+            - Actives in Event
+
+    INIT:
+        Event -- starts the event
+            - Starts an Event
+
+        delete -- auto-matically deletes a function
+            - Goes to _0functiontbl
+            - Actives in Event
+
+        Fill -- auto-matically adds a function inside **Event**
+            - Goes to _0functiontbl
+            - Actives in Event
+
+        Destroy -- Destroy all of the event itself and cleans up the functions.
+            - Goes to connectionTable
+            - Actives in Event
+
+        Disconnect -- Destroys an event auto-matically
+            - Goes to connectionTable
+            - Actives in Event
+]]
+
+local connectionTable = {} :: {RBXScriptSignal | RBXScriptConnection}
+
+local globalTableF = {} :: {() -> ()}
 
 local Kiraps = {} :: {}
 Kiraps.__index = Kiraps
@@ -11,7 +50,7 @@ Kiraps.__index = Kiraps
 function Kiraps.new(name_space: string | number, con: RBXScriptSignal) : ()
     assert(typeof(con) == "RBXScriptSignal", `{con} isn't a signal!`)
 
-    if connectionTable[con] == nil then
+    if connectionTable[name_space] == nil then
         if connectionTable[name_space] then
             return error("Cannot have the same name")
         end
@@ -24,7 +63,7 @@ function Kiraps.new(name_space: string | number, con: RBXScriptSignal) : ()
         _0functiontbl = {} :: {},
         _0namespace = name_space :: string or nil,
         _0connection = connectionTable[name_space] :: RBXScriptSignal,
-        _0old = nil :: RBXScriptSignal | nil
+        _0isUsing = nil :: RBXScriptSignal | nil
         },Kiraps
     )
 end
@@ -40,11 +79,14 @@ function Kiraps:addGlobal(name_space: string | number, name_thread: string, fn: 
         globalTableF[name_space] = {}
         globalTableF[name_space]._0globaltbl = {}
         globalTableF[name_space]._0globaltbl[name_thread] = fn
+
+        print(globalTableF)
         return
     end
 
     if globalTableF[name_space][name_thread] == nil then
         globalTableF[name_space]._0globaltbl[name_thread] = fn
+        print(globalTableF)
         return
     end
 
@@ -53,10 +95,7 @@ end
 
 function Kiraps:deleteGlobal(name_space: string | number, name_thread : string)
     if globalTableF[name_space] ~= nil then
-        if globalTableF[name_space]._0globaltbl[name_thread] then
-            globalTableF[name_space]._0globaltbl[name_thread] = nil
-            print(globalTableF)
-        end
+        globalTableF[name_space]._0globaltbl[name_thread] = nil
     end
 
     return self
@@ -72,28 +111,42 @@ function Kiraps:delete()
     return self
 end
 
-function Kiraps:_tableFunc(table: {})
+function Kiraps:_tableFunc(table: {}, ...)
+    if table == nil then
+        return
+    end
+
     for _, _0functiontbl in table do
         if _0functiontbl then
-            _0functiontbl()
+            _0functiontbl(...)
         end
     end
+end
+
+function Kiraps:returnFunction(types: boolean)
+    if types ~= nil then
+        print(globalTableF)
+
+        return
+    end
+
+    print(self._0functiontbl)
 end
 
 function Kiraps:Event(fn:() -> ()) : ()
     assert(type(fn) == "function", `{fn} is not a function!`)
 
-    if self._0connection and typeof(self._0connection) == "RBXScriptSignal" then
+    if connectionTable[self._0namespace] ~= nil and typeof(connectionTable[self._0namespace]) == "RBXScriptSignal" then
 
-        if self._0old and self._0old.Connected then
+        --[[if connectionTable[self._0namespace] and connectionTable[self._0namespace].Connected then
             return error("Cannot connect more than one Event.")
-        end
+        end]]
         
-        self._0old = connectionTable[self._0namespace]:Connect(function(...)
+        connectionTable[self._0namespace] = connectionTable[self._0namespace]:Connect(function(...)
             fn(...)
-            if self._0functiontbl[self._0namespace] == nil then return end
-            self:_tableFunc(self._0functiontbl[self._0namespace])
-            self:_tableFunc(globalTableF[self._0namespace]._0globaltbl)
+
+            self:_tableFunc(self._0functiontbl[self._0namespace], ...)
+            self:_tableFunc(globalTableF[self._0namespace]._0globaltbl, ...)
         end)
     end
 
@@ -110,22 +163,41 @@ function Kiraps:Fill(fn:() -> ()): ()
     return self
 end
 
-function Kiraps:Destroy(): ()
-    self._0old:Disconnect();
-    self._0old = nil;
-    self._0connection = nil;
+function Kiraps:Disconnect(name_space: string | number)
+    if name_space ~= nil then
+        print(connectionTable[name_space])
+        connectionTable[name_space]:Disconnect()
+        connectionTable[name_space] = nil
+        globalTableF[name_space] = nil
 
-    if globalTableF[self._0namespace] then
-        for _, threads in globalTableF[self._0namespace]._0globaltbl do
-            coroutine.close(threads)
-        end
-        globalTableF[self._0namespace] = nil;
+        print(connectionTable, globalTableF)
+        
+        return
     end
+
+    connectionTable[self._0namespace]:Disconnect()
+    connectionTable[self._0namespace] = nil
+    globalTableF[self._0namespace] = nil
+    self._0functiontbl[self._0namespace] = nil
+
+    return self
+end
+
+function Kiraps:Destroy(): ()
+    if self == nil then
+        return
+    end
+
+    connectionTable[self._0namespace]:Disconnect();
+    connectionTable[self._0namespace] = nil;
+
+    globalTableF[self._0namespace] = nil;
 
     connectionTable[self._0namespace] = nil;
     self._0functiontbl[self._0namespace] = nil;
     self._0namespace = nil;
-
+    
+    table.clear(self)
     return self
 end
 
