da904e5784a6308ce154eafc04806d0a84c50758
raydjs <68088905+raydjs@users.noreply.github.com>
2023-10-07 19:39:20 -0600

    Update KiroNex.lua

--- KiroNex.lua
+++ KiroNex.lua
@@ -1,71 +1,46 @@
---[[
-    API:
-    >> function: {
-        
-        self:init(TABLE, SELECTOR) 
-            -- FILLS THE CLIENT TABLE MODULE /  SERVER TABLE MODULE
-                * TABLE: [any]
-                    -- Path or a Folder or a Class to path to.
-                * SELECTOR: [INDEXER]
-                 -- must select through
-                    -- Util
-                    -- Script
-                    -- Unknown
-        self:load(TABLE, PATTERN)
-            -- REQUIRES ALL THE MODULESCRIPTS
-                * TABLE: [any] :: TABLE
-                    -- Path or a Folder or a table to path to.
-                * PATTERN: [any] :: TABLE
-                    -- A table to match the pattern string names of the ModuleScript that was filled.
-
-        EXAMPLES:
-            self:init(ServerScriptService.Folder, "Script"):load("Script", {"Stuff_folder", "load_folder"})
-
-            self:init(ServerScriptService.Folder, "Util")
-    }
-]]
-local KiroNet = {
-    Modules = {
-        Util = {},
-        Script = {},
-        Unknown = {}
-    }
+local KiroNex = {
+    Modules = {},
 }
 
-function KiroNet:init(Table : any, Select : string)
-    if Select == nil then
-        Select = "Unknown"
-    end
 
-    for _, Modules in Table:GetDescendants() do
-        local IsModuleScript = Modules:IsA("ModuleScript")
+function KiroNex.init(container, match : table)
+    for _, module in container:GetDescendants() do
+        local isModule = module:IsA("ModuleScript")
+        for _, matched in match do
+            if isModule and module.Name:match(`{matched}$`) then
+                if KiroNex.Modules[module.Name] ~= nil then return end
 
-        if IsModuleScript then
-            self.Modules[Select][Modules.Name] = Modules
-        end
-    end
-
-    return self
-end
-
-function KiroNet:load(Select : string, pattern : any)
-
-    for _, Modules in self.Modules[Select] do
-        task.spawn(function()
-            for _, Array in pattern do
-                if table.find(pattern, Modules.Name:match(`{Array}$`)) then
-                    if typeof(require(Modules)) == "table" then
-                        require(Modules)
-            
-                    elseif typeof(require(Modules)) == "function" then
-                        require(Modules)()
-                    end
+                if (typeof(require(module)) == "table") then
+                    KiroNex.Modules[module.Name] = require(module)
+                elseif (typeof(require(module)) == "function") then
+                    KiroNex.Modules[module.Name] = require(module)()
                 end
             end
-        end)
+        end
     end
-    
-    return self
 end
 
-return KiroNet
+function KiroNex.add(container_or_file)
+    if KiroNex.Modules[container_or_file.Name] ~= nil then return end
+    if typeof(container_or_file) == 'table' then
+
+        for _, module in container_or_file do
+            local isModule = module:IsA("ModuleScript")
+
+            if isModule then
+                if KiroNex.Modules[module.Name] ~= nil then return end
+                KiroNex.Modules[module.Name] = require(isModule)
+            end
+        end
+
+        return;
+    end
+    KiroNex.Modules[container_or_file.Name] = require(container_or_file)
+end
+
+function KiroNex.use(ModuleName: string & 'Example: KiroNex:use("Hitbox")')
+    if KiroNex.Modules[ModuleName] == nil then return end
+    return KiroNex.Modules[ModuleName]
+end
+
+return KiroNex
